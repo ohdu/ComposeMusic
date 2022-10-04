e1ac190e50f5dc3fe852fe2b164c4e1555fe0eee
ohdu <lidaryl@163.com>
2022-10-04 23:36:07 +0800

    :tada: Change the material to material3

--- app/src/main/java/com/ohdu/music/MainActivity.kt
+++ app/src/main/java/com/ohdu/music/MainActivity.kt
@@ -1,40 +1,63 @@
 package com.ohdu.music
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ohdu.music.ui.theme.ComposeMusicTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeMusicTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Scaffold(bottomBar = { MainBottomNavigation() }) {
+
                 }
+
             }
         }
     }
 }
 
+@Preview(name = "首页底部导航栏")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainBottomNavigation() {
+    NavigationBar {
+        NavigationBarItem(selected = false, onClick = {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMusicTheme {
-        Greeting("Android")
+        }, icon = {
+            Icon(Icons.Default.Home, contentDescription = "")
+        }, label = {
+            Text(text = "首页")
+        }, alwaysShowLabel = false
+        )
+        NavigationBarItem(selected = false, onClick = {
+
+        }, icon = {
+            Icon(Icons.Default.Add, contentDescription = "")
+        }, label = {
+            Text(text = "音乐")
+        }, alwaysShowLabel = false
+        )
+        NavigationBarItem(selected = false, onClick = {
+
+        }, icon = {
+            Icon(Icons.Default.Menu, contentDescription = "")
+        }, label = {
+            Text(text = "我的")
+        }, alwaysShowLabel = false
+        )
     }
 }
--- app/src/main/java/com/ohdu/music/ui/theme/Theme.kt
+++ app/src/main/java/com/ohdu/music/ui/theme/Theme.kt
@@ -1,30 +1,26 @@
 package com.ohdu.music.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200, secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+private val LightColorPalette = lightColorScheme(
+    primary = Purple500, secondary = Teal200
 
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    /* Other default colors to override
+background = Color.White,
+surface = Color.White,
+onPrimary = Color.White,
+onSecondary = Color.Black,
+onBackground = Color.Black,
+onSurface = Color.Black,
+*/
 )
 
 @Composable
@@ -36,9 +32,6 @@ fun ComposeMusicTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        typography = Typography, shapes = Shapes, content = content, colorScheme = colors
     )
 }
